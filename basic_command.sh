996c660ae86c5ac110156783935623a98f679883
Apichai Aimwong <apichai.a@kkumail.com>
2026-07-08 02:39:04 +0000

    add basic_command.sh

---
+++ basic_command.sh
@@ -0,0 +1,6 @@
+git add . #adds all changes to the staging area
+
+git commit -m "Your commit message" #commits the changes with a message
+
+git push origin <remote-branch-name> #pushes the changes to the specified remote branch
+
