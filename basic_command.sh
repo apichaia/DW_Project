6e16cd8d5beb0a23edb1dce34c5aa834f1789e2b
Apichai Aimwong <apichai.a@kkumail.com>
2026-07-08 03:17:01 +0000

    Install python libralies

--- basic_command.sh
+++ basic_command.sh
@@ -5,3 +5,7 @@ git commit -m "Your commit message" #commits the changes with a message
 git push origin <remote-branch-name> #pushes the changes to the specified remote branch
 
 git switch <branch-name> #switches to the specified branch
+
+# pip command 
+#freeze the current environment's packages and their versions into a requirements.txt file
+pip freeze > requirements.txt
