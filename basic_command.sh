9ee20cee5812067f61dfbbefcd9fa98756e19520
Apichai Aimwong <apichai.a@kkumail.com>
2026-07-08 03:00:12 +0000

    update: pip

---
+++ basic_command.sh
@@ -0,0 +1,7 @@
+git add . #adds all changes to the staging area
+
+git commit -m "Your commit message" #commits the changes with a message
+
+git push origin <remote-branch-name> #pushes the changes to the specified remote branch
+
+git switch <branch-name> #switches to the specified branch
